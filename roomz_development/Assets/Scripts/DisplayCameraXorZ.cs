c5dea1e121dd79b7f06f8f8b85453939354b1140
Jian Shen Tan <jianshentan@mail.com>
2012-06-30 23:01:37 +0800

    camera can now rotate left all the way (previously, it could only go left, then back to right). The play also turns left first before camera switches

--- roomz_development/Assets/Scripts/DisplayCameraXorZ.cs
+++ roomz_development/Assets/Scripts/DisplayCameraXorZ.cs
@@ -3,28 +3,52 @@ using System.Collections;
 
 public class DisplayCameraXorZ : MonoBehaviour {
 	
-	public GameObject cameraX;
-	public GameObject cameraZ;
-		
-	// Use this for initialization
-	void Start () {
-		cameraX = transform.FindChild("CameraX").gameObject;
-		cameraZ = transform.FindChild("CameraZ").gameObject;
-		cameraX.SetActiveRecursively(true);
-		cameraZ.SetActiveRecursively(false);
-	}
+	public GameObject cameraX_1;
+	public GameObject cameraZ_2;
+	public GameObject cameraX_3;
+	public GameObject cameraZ_4;	
 	
-	// Update is called once per frame
+	public Transform player;
+	
+	void Start () {
+		cameraX_1 = transform.FindChild("CameraX_1").gameObject;
+		cameraZ_2 = transform.FindChild("CameraZ_2").gameObject;
+		cameraX_3 = transform.FindChild("CameraX_3").gameObject;
+		cameraZ_4 = transform.FindChild("CameraZ_4").gameObject;
+		
+		cameraX_1.SetActiveRecursively(true);
+		cameraZ_2.SetActiveRecursively(false);
+		cameraX_3.SetActiveRecursively(false);
+		cameraZ_4.SetActiveRecursively(false);
+	}
+
 	void Update () {
 		if(Input.GetKeyUp("space")){
-			if(cameraX.active){
-				cameraX.SetActiveRecursively(false);
-				cameraZ.SetActiveRecursively(true);
-			}else{
-				cameraX.SetActiveRecursively(true);
-				cameraZ.SetActiveRecursively(false);
-			}
+			StartCoroutine(rotateLeft());
+		}	
+	}
+	
+	IEnumerator rotateLeft(){
+		player.Rotate(0,-90f,0);	
+		yield return new WaitForSeconds(1);
+
+		if(cameraX_1.active){
+			cameraX_1.SetActiveRecursively(false);
+			cameraZ_2.SetActiveRecursively(true);
+		}
+		else if(cameraZ_2.active){
+			cameraZ_2.SetActiveRecursively(false);
+			cameraX_3.SetActiveRecursively(true);
+		}
+		else if(cameraX_3.active){
+			cameraX_3.SetActiveRecursively(false);
+			cameraZ_4.SetActiveRecursively(true);
+		}
+		else if(cameraZ_4.active){
+			cameraZ_4.SetActiveRecursively(false);
+			cameraX_1.SetActiveRecursively(true);
 		}
 		
 	}
+	
 }
--- roomz_development/Assets/Scripts/PlayerMove.cs
+++ roomz_development/Assets/Scripts/PlayerMove.cs
@@ -5,31 +5,13 @@ public class PlayerMove : MonoBehaviour {
 	
 	public DisplayCameraXorZ cam;
 	public float speed = 500;
-	
-	// Use this for initialization
-	void Start () {
-		//DisplayCameraXorZ cam = cameraXZ.GetComponent<DisplayCameraXorZ>();
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if(cam.cameraZ.active)
-		{
-			if(Input.GetKey("up")){
-				gameObject.transform.Translate(0, 0,speed * Time.deltaTime);
-			}
-			if(Input.GetKey("down")){
-				gameObject.transform.Translate(0, 0,-speed * Time.deltaTime);
-			}
-		}
-		else
-		{
 			if(Input.GetKey("up")){
 				gameObject.transform.Translate(speed * Time.deltaTime, 0,0);
 			}
 			if(Input.GetKey("down")){
 				gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
 			}
-		}
 	}
 }
